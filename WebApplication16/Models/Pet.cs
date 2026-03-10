90f70743e1a2c16ee0b4afdf1de846bc3c2b690a
Yevhen <yevhen_danylchenko@ukr.net>
2026-03-10 20:49:53 +0200

    Models added

--- WebApplication16/Models/Pet.cs
+++ WebApplication16/Models/Pet.cs
@@ -2,5 +2,30 @@
 {
     public class Pet
     {
+        public int Id { get; set; }
+
+        // Основні дані
+        public string Name { get; set; } = string.Empty;
+        public string Species { get; set; } = string.Empty; // Наприклад: Dog, Cat
+        public string Breed { get; set; } = string.Empty; // Порода
+        public DateTime? BirthDate { get; set; }
+        public string Gender { get; set; } = string.Empty;
+
+        // Медична інформація
+        public string? Vaccinations { get; set; }
+        public string? Allergies { get; set; }
+        public string? ChronicDiseases { get; set; }
+
+        // Зв’язки з іншими сутностями
+        public string? PatientId { get; set; }
+        public Patient? Patients { get; set; } // Власник тварини
+        public ICollection<Disease> Diseases { get; set; }
+        public ICollection<Therapy> Therapies { get; set; }
+
+        // Метадані
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsActive { get; set; } = true;
+
     }
 }
--- WebApplication16/Models/Therapy.cs
+++ WebApplication16/Models/Therapy.cs
@@ -2,5 +2,13 @@
 {
     public class Therapy
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty; // Назва терапії
+        public string? Description { get; set; }        // Опис терапії
+        public int DiseaseId { get; set; }             // Зовнішній ключ до хвороби
+        public Disease Disease { get; set; }           // Навігаційна властивість до хвороби
+        // Метадані
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
     }
 }
