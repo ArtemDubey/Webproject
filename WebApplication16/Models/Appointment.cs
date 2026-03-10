4c15b4c5fa27d4779ec2c5c373bb108738abc70f
Yevhen <yevhen_danylchenko@ukr.net>
2026-03-10 20:52:04 +0200

    Models

--- WebApplication16/Models/Appointment.cs
+++ WebApplication16/Models/Appointment.cs
@@ -2,5 +2,29 @@
 {
     public class Appointment
     {
+        public int Id { get; set; }
+
+        // Основні дані
+        public DateTime Date { get; set; } // дата та час прийому
+        public string? Reason { get; set; } // причина візиту (симптоми, консультація)
+
+        // Зв’язки з іншими сутностями
+        public string? DoctorId { get; set; }
+        public Doctor? Doctor { get; set; }
+
+        public string? PatientId { get; set; }
+        public Patient? Patient { get; set; }
+
+        public int? PetId { get; set; }
+        public Pet? Pet { get; set; }
+
+        // Статус прийому
+        public string Status { get; set; } = "Scheduled";
+        // Наприклад: Scheduled, Completed, Cancelled
+
+        // Метадані
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
     }
 }
